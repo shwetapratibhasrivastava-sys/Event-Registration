259780c4dda96a725f0decd4bc30d958261532a5
Shweta Srivastava <shwetapratibhasrivastava@gmail.com>
2026-05-28 22:51:30 +0530

    middle ware added

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,14 +1,14 @@
 import express from "express"
 import { createUser, deleteUser, getByIdUser, getUser, updateUser } from "../controllers/userController.js"
-
+import authMiddlware from "../middleware/authMiddleware.js"
 
 const userRoute=express.Router()
 
 userRoute.post("/create",createUser)
-userRoute.get("/get",getUser)
-userRoute.get("/get/:id",getByIdUser)
-userRoute.put("/update/:id",updateUser)
-userRoute.delete("/delete/:id",deleteUser)
+userRoute.get("/get",authMiddlware,getUser)
+userRoute.get("/get/:id",authMiddlware,getByIdUser)
+userRoute.put("/update/:id",authMiddlware,updateUser)
+userRoute.delete("/delete/:id",authMiddlware,deleteUser)
 
 
 export default userRoute
